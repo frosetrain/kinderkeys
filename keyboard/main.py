cd1bf53986d320b0af9c2be0d8b159f2c794362e
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2025-03-29 17:23:25 +0800

    Add keyboard controller

---
+++ keyboard/main.py
@@ -0,0 +1,20 @@
+from pyautogui import press
+from uvloop import run
+from websockets.asyncio.client import connect
+
+
+async def main():
+    """Listen for keypresses."""
+    uri = "ws://localhost:1968"
+    async with connect(uri) as websocket:
+        await websocket.send("keyboard")
+        async for message in websocket:
+            print(message)
+            args = message.split(" ")
+            if args[0] == "press":
+                key = args[1]
+                press(key)
+
+
+if __name__ == "__main__":
+    run(main())
